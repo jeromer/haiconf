cf410d02087d6099c26f4de711b80c213391793e
Jérôme Renard <jerome.renard@gmail.com>
2013-11-20 10:45:47 +0100

    Commented non implemented prototypes

--- prototypes/function_prototypes.lua
+++ prototypes/function_prototypes.lua
@@ -1,11 +1,14 @@
-function Main()
-    RuntimeConfig({
-        RollbackOnError = true,
-    })
+-- /!\ READ THIS PLEASE /!\
+-- If the code you read below is commented, that means it is not available in
+-- haiconf yet
+--
 
-    --
-    -- Core commands
-    --
+function Main()
+    -- Will be implemented later
+    -- RuntimeConfig({
+    --    RollbackOnError = true,
+    -- })
+
     Directory({
         Path    = "/path/to/directory",
         Mode    = 0755,
@@ -24,22 +27,19 @@ function Main()
         Template = "/path/to/template.ext"
     })
 
-    -- related to RollbackOnError
+    -- related to RollbackOnError, not implemented yet
     -- if execution of commands below fails for some reason
     -- the runtime will rollback only up to this point
-    Checkpoint({
-        Id = "Checkpoint 1",
-    })
+    -- Checkpoint({
+    --    Id = "Checkpoint 1",
+    -- })
 
     AptGet({
         Method = "install",
-
         -- defined here:
         Packages = {"vim", "mutt", "cowsay"},
-
         -- or alternatively:
-        PackageFromSource = "/path/to/packages.to.install.txt",
-
+        -- PackageFromSource = "/path/to/packages.to.install.txt",
         -- automatically added to the apt-get call
         ExtraOptions = {
             "--download-only",
@@ -48,65 +48,85 @@ function Main()
         }
     })
 
-    Cron({
-        Command = "/path/to/ntpdate",
-        Ensure = "present",
-
-        Env = {
-            PATH     = "$PATH:/usr/bin/foo",
-            ENV_VAR2 = "foo-bar",
-        },
-
-        Schedule = {
-            -- yearly / monthly / weekly / daily / hourly / reboot etc
-            Predefined = "yearly",
-
-            -- alternatively :
-            WeekDay  = "*",
-            Month    = "*",
-            MonthDay = "*",
-            Hour     = "*",
-            Minute   = "*",
-        },
-
-        RunAs = "root",
+    AptGet({
+        Method = "update",
     })
 
-    Service({
-        Ensure = "running",
-        Name = "foo",
-        -- XXX : I need to figure out the rest
-    })
-
-    Exec({
-        Command = "/usr/bin/foo",
-        ExecutionDir = "/home/someuser",
-        RunAs = "root",
-
-        Env = {
-            PATH     = "$PATH:/usr/bin/foo",
-            ENV_VAR2 = "foo-bar",
+    AptGet({
+        Method = "remove",
+        -- defined here:
+        Packages = {"vim", "mutt", "cowsay"},
+        -- or alternatively:
+        -- PackageFromSource = "/path/to/packages.to.remove.txt",
+        -- automatically added to the apt-get call
+        ExtraOptions = {
+            "--purge",
         }
     })
 
-    --
-    -- small utilities
-    --
-    Curl({
-        Url = "http://some.url/path.to.pkg",
-    })
-
-    TarGz({
-        From = "/path/to/from/dir",
-        Source = "/path/to/dest/dir.tar.gz",
-    })
-
-    UnTarGz({
-        From = "/path/to/from/dir.tar.gz",
-        Source = "/path/to/dest/dir",
-    })
-
-    Touch({
-        File = "/path/to/file",
-    })
+-- ----------------------------------------
+-- Everything below is not implement (yet)
+-- ----------------------------------------
+--
+--    Cron({
+--        Command = "/path/to/ntpdate",
+--        Ensure = "present",
+--
+--        Env = {
+--            PATH     = "$PATH:/usr/bin/foo",
+--            ENV_VAR2 = "foo-bar",
+--        },
+--
+--        Schedule = {
+--            -- yearly / monthly / weekly / daily / hourly / reboot etc
+--            Predefined = "yearly",
+--
+--            -- alternatively :
+--            WeekDay  = "*",
+--            Month    = "*",
+--            MonthDay = "*",
+--            Hour     = "*",
+--            Minute   = "*",
+--        },
+--
+--        RunAs = "root",
+--    })
+--
+--    Service({
+--        Ensure = "running",
+--        Name = "foo",
+--        -- XXX : I need to figure out the rest
+--    })
+--
+--    Exec({
+--        Command = "/usr/bin/foo",
+--        ExecutionDir = "/home/someuser",
+--        RunAs = "root",
+--
+--        Env = {
+--            PATH     = "$PATH:/usr/bin/foo",
+--            ENV_VAR2 = "foo-bar",
+--        }
+--    })
+--
+--    --
+--    -- small utilities
+--    --
+--    Curl({
+--        Url = "http://some.url/path.to.pkg",
+--    })
+--
+--    TarGz({
+--        From = "/path/to/from/dir",
+--        Source = "/path/to/dest/dir.tar.gz",
+--    })
+--
+--    UnTarGz({
+--        From = "/path/to/from/dir.tar.gz",
+--        Source = "/path/to/dest/dir",
+--    })
+--
+--    Touch({
+--        File = "/path/to/file",
+--    })
 end
