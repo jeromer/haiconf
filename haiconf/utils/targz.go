b8e704084839c3bc3ac72f9e4d306f18d5e52abc
Jérôme Renard <jerome.renard@gmail.com>
2013-12-13 22:06:45 +0100

    Added TarGz

---
+++ haiconf/utils/targz.go
@@ -0,0 +1,215 @@
+package utils
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"github.com/jeromer/haiconf/haiconf"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Usage in a haiconf file
+//
+// TarGz({
+//    Source = "/path/to/dir/",
+//    Dest = "/path/to/tarball.tar.gz",
+// })
+
+type TarGz struct {
+	source string
+	dest   string
+
+	rc *haiconf.RuntimeConfig
+}
+
+func (t *TarGz) SetDefault(rc *haiconf.RuntimeConfig) error {
+	t.rc = rc
+	return nil
+}
+
+func (t *TarGz) SetUserConfig(args haiconf.CommandArgs) error {
+	err := t.setSource(args)
+	if err != nil {
+		return err
+	}
+
+	err = t.setDest(args)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TarGz) Run() error {
+	haiconf.Output(t.rc, "Extracting %s to %s", t.source, t.dest)
+
+	return tarGz(t.source, t.dest)
+}
+
+func (t *TarGz) setSource(args haiconf.CommandArgs) error {
+	s, _ := haiconf.CheckString("Source", args)
+	if len(s) == 0 {
+		return haiconf.NewArgError("Source must be provided", args)
+	}
+
+	f, err := os.Open(s)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return haiconf.NewArgError("Source does not exist", args)
+		}
+	}
+	defer f.Close()
+
+	t.source = s
+	return nil
+}
+
+func (t *TarGz) setDest(args haiconf.CommandArgs) error {
+	d, _ := haiconf.CheckString("Dest", args)
+	if len(d) == 0 {
+		return haiconf.NewArgError("Dest must be provided", args)
+	}
+
+	if !strings.HasSuffix(d, ".tar.gz") {
+		return haiconf.NewArgError("No tarball name provided", args)
+	}
+
+	id, err := isDir(filepath.Dir(d))
+	if err != nil {
+		return err
+	}
+
+	if !id {
+		return haiconf.NewArgError(d+" is not a directory", args)
+	}
+
+	t.dest = d
+	return nil
+}
+
+func tarGz(source string, dest string) error {
+	archive, err := createTar(source)
+	if err != nil {
+		return err
+	}
+
+	gzBuff, err := gz(archive)
+	if err != nil {
+		return err
+	}
+
+	return writeFile(gzBuff, dest)
+}
+
+func createTar(source string) ([]byte, error) {
+	buff := new(bytes.Buffer)
+	nilBuff := []byte(nil)
+	var err error
+
+	tarWriter := tar.NewWriter(buff)
+
+	// texas ranger :D
+	walker := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		fi, err := os.Lstat(path)
+		if err != nil {
+			return err
+		}
+
+		mode := fi.Mode()
+
+		link := ""
+		if mode&os.ModeSymlink != 0 {
+			link, err = os.Readlink(path)
+			if err != nil {
+				return err
+			}
+		}
+
+		hdr, err := tar.FileInfoHeader(fi, link)
+		if err != nil {
+			return err
+		}
+		hdr.Name = path
+
+		err = tarWriter.WriteHeader(hdr)
+		if err != nil {
+			return err
+		}
+
+		if !mode.IsRegular() {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		fbuff, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+		f.Close()
+
+		_, err = tarWriter.Write(fbuff)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	err = filepath.Walk(source, walker)
+	if err != nil {
+		return nilBuff, err
+	}
+
+	err = tarWriter.Close()
+	if err != nil {
+		return nilBuff, err
+	}
+
+	return buff.Bytes(), nil
+}
+
+func gz(buff []byte) ([]byte, error) {
+	gzBuff := new(bytes.Buffer)
+	nilBuff := []byte(nil)
+
+	writer := gzip.NewWriter(gzBuff)
+	_, err := writer.Write(buff)
+	if err != nil {
+		return nilBuff, err
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		return nilBuff, err
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return nilBuff, err
+	}
+
+	return gzBuff.Bytes(), nil
+}
+
+func writeFile(buff []byte, name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(buff)
+	return err
+}
---
+++ haiconf/utils/targz_test.go
@@ -0,0 +1,213 @@
+package utils
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"github.com/jeromer/haiconf/haiconf"
+	"io"
+	"io/ioutil"
+	. "launchpad.net/gocheck"
+	"os"
+)
+
+type TarGzTestSuite struct {
+	t *TarGz
+}
+
+var (
+	_ = Suite(&TarGzTestSuite{})
+)
+
+func (s *TarGzTestSuite) SetUpTest(c *C) {
+	s.t = new(TarGz)
+	err := s.t.SetDefault(&dummyRuntimeConfig)
+	c.Assert(err, IsNil)
+}
+
+func (s *TarGzTestSuite) TearDownTest(c *C) {
+	s.t.source = ""
+	s.t.dest = ""
+}
+
+func (s *TarGzTestSuite) TestSetSource_Empty(c *C) {
+	args := haiconf.CommandArgs{
+		"Source": "",
+	}
+
+	err := s.t.setSource(args)
+	c.Assert(err, ErrorMatches, "Source must be provided. (.*)")
+	c.Assert(s.t.source, Equals, "")
+}
+
+func (s *TarGzTestSuite) TestSetSource_FileDoesNotExist(c *C) {
+	args := haiconf.CommandArgs{
+		"Source": "/foo/bar/tarball.tar.gz",
+	}
+
+	err := s.t.setSource(args)
+	c.Assert(err, ErrorMatches, "Source does not exist. (.*)")
+	c.Assert(s.t.source, Equals, "")
+}
+
+func (s *TarGzTestSuite) TestSetSource_FileExists(c *C) {
+	tmpDir := c.MkDir()
+	args := haiconf.CommandArgs{
+		"Source": tmpDir,
+	}
+
+	err := s.t.setSource(args)
+	c.Assert(err, IsNil)
+	c.Assert(s.t.source, Equals, tmpDir)
+}
+
+func (s *TarGzTestSuite) TestSetDest_Empty(c *C) {
+	args := haiconf.CommandArgs{
+		"Dest": "",
+	}
+
+	err := s.t.setDest(args)
+	c.Assert(err, ErrorMatches, "Dest must be provided. (.*)")
+	c.Assert(s.t.dest, Equals, "")
+}
+
+func (s *TarGzTestSuite) TestSetDest_DirDoesNotExist(c *C) {
+	tmpDir := c.MkDir() + "/foo"
+	args := haiconf.CommandArgs{
+		"Dest": tmpDir + "/tarball.tar.gz",
+	}
+
+	err := s.t.setDest(args)
+	c.Assert(err, ErrorMatches, "stat "+tmpDir+": no such file or directory")
+	c.Assert(s.t.dest, Equals, "")
+}
+
+func (s *TarGzTestSuite) TestSetDest_Exists(c *C) {
+	tmpDir := c.MkDir()
+	tarball := "tarball.tar.gz"
+	fullPath := tmpDir + "/" + tarball
+	args := haiconf.CommandArgs{
+		"Dest": fullPath,
+	}
+
+	err := s.t.setDest(args)
+	c.Assert(err, IsNil)
+	c.Assert(s.t.dest, Equals, fullPath)
+}
+
+func (s *TarGzTestSuite) TestSetDest_NoTarballName(c *C) {
+	args := haiconf.CommandArgs{
+		"Dest": c.MkDir(),
+	}
+
+	err := s.t.setDest(args)
+	c.Assert(err, ErrorMatches, "No tarball name provided. (.*)")
+	c.Assert(s.t.dest, Equals, "")
+}
+
+func (s *TarGzTestSuite) TestSetUserConfig_Complete(c *C) {
+	tmpDir := c.MkDir()
+
+	source := tmpDir
+	dest := tmpDir + "/foo.tar.gz"
+
+	args := haiconf.CommandArgs{
+		"Source": source,
+		"Dest":   dest,
+	}
+
+	err := s.t.SetUserConfig(args)
+	c.Assert(err, IsNil)
+	c.Assert(s.t.source, Equals, source)
+	c.Assert(s.t.dest, Equals, dest)
+}
+
+func (s *TarGzTestSuite) TestCreateTar(c *C) {
+	buff, err := createTar("./fixtures/")
+	c.Assert(err, IsNil)
+	c.Assert(len(buff) > 0, Equals, true)
+
+	rdr := bytes.NewReader(buff)
+	tr := tar.NewReader(rdr)
+
+	var obtained []*tar.Header
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		obtained = append(obtained, hdr)
+	}
+
+	expected := []*tar.Header{
+		&tar.Header{
+			Name:     "./fixtures/",
+			Typeflag: tar.TypeDir,
+		},
+		&tar.Header{
+			Name:     "fixtures/dir",
+			Typeflag: tar.TypeDir,
+		},
+		&tar.Header{
+			Name:     "fixtures/dir/file.txt",
+			Typeflag: tar.TypeReg,
+		},
+		&tar.Header{
+			Name:     "fixtures/dir/hardlink",
+			Typeflag: tar.TypeReg,
+		},
+		&tar.Header{
+			Name:     "fixtures/dir/symlink",
+			Typeflag: tar.TypeSymlink,
+		},
+	}
+
+	c.Assert(len(obtained), Equals, len(expected))
+	for i, hdr := range obtained {
+		c.Assert(hdr.Name, Equals, expected[i].Name)
+		c.Assert(hdr.Typeflag, Equals, expected[i].Typeflag)
+	}
+}
+
+func (s *TarGzTestSuite) TestGz(c *C) {
+	expected := []byte("foo")
+
+	gzipped, err := gz(expected)
+	c.Assert(err, IsNil)
+	c.Assert(len(gzipped) > 0, Equals, true)
+
+	reader, err := gzip.NewReader(bytes.NewBuffer(gzipped))
+	c.Assert(err, IsNil)
+	defer reader.Close()
+
+	buff, err := ioutil.ReadAll(reader)
+	c.Assert(err, IsNil)
+	c.Assert(buff, DeepEquals, expected)
+}
+
+func (s *TarGzTestSuite) TestRun_TarGz(c *C) {
+	source := "./fixtures"
+	dest := c.MkDir() + "/fixtures.tar.gz"
+
+	args := haiconf.CommandArgs{
+		"Source": source,
+		"Dest":   dest,
+	}
+
+	err := s.t.SetUserConfig(args)
+	c.Assert(err, IsNil)
+	c.Assert(s.t.source, Equals, source)
+	c.Assert(s.t.dest, Equals, dest)
+
+	err = s.t.Run()
+	c.Assert(err, IsNil)
+
+	f, err := os.Open(dest)
+	c.Assert(err, IsNil)
+	defer f.Close()
+}
